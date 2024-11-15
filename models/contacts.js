86c1e3760081b3533a0cc18fcd534b8186feb8e0
cygan3107 <krzysztof.malon@onet.pl>
2024-11-15 16:27:38 +0100

    done

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,83 @@
 // const fs = require('fs/promises')
 
-const listContacts = async () => {}
+const fs = require("fs").promises;
+const path = require("path");
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Error reading or parsing file:", error);
+    throw error;
+  }
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const contact = contacts.find((contact) => contact.id === contactId);
+    return contact;
+  } catch (error) {
+    console.error("Error reading or parsing file:", error);
+    throw error;
+  }
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const contact = contacts.find((contact) => contact.id === contactId);
+    const newContacts = contacts.filter((contact) => contact.id !== contactId);
+    await fs.writeFile(contactsPath, JSON.stringify(newContacts));
+    return contact;
+  } catch (error) {
+    console.error("Error reading or parsing file:", error);
+    throw error;
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (body) => {
+  try {
+    const { name, email, phone } = body;
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const id = contacts.length + 1;
+    const contactToAdd = {
+      id: String(id),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(contactToAdd);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  } catch (error) {
+    console.error("Error reading or parsing file:", error);
+    throw error;
+  }
+};
+
+const updateContact = async (contactId, body) => {
+  try {
+    const { name, email, phone } = body;
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const contact = contacts.find((contact) => contact.id === contactId);
+    if (contact) {
+      contact.name = name;
+      contact.email = email;
+      contact.phone = phone;
+    }
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return contact;
+  } catch (error) {
+    console.error("Error reading or parsing file:", error);
+    throw error;
+  }
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +85,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
